54dfd4c5693ca1cf04847bbf12fa96b380c2ba6c
nodssor <49564142+nodssor@users.noreply.github.com>
2022-02-12 18:53:08 -0500

    Update boostrap.sh

--- boostrap.sh
+++ boostrap.sh
@@ -1,5 +1,5 @@
 # This is to be run directly on new Linux installs
-# ex: curl -Lks 
+# ex: curl -Lks https://raw.githubusercontent.com/nodssor/public-files/main/boostrap.sh
 git init --bare $HOME/.dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 dotfiles config --local status.showUntrackedFiles no
