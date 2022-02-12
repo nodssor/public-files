ac89c73c6ca15eb34f3ac359d10c71c52d4133d9
nodssor <49564142+nodssor@users.noreply.github.com>
2022-02-12 18:51:01 -0500

    Create boostrap.sh

---
+++ boostrap.sh
@@ -0,0 +1,6 @@
+# This is to be run directly on new Linux installs
+# ex: curl -Lks 
+git init --bare $HOME/.dotfiles
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+dotfiles config --local status.showUntrackedFiles no
+echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
