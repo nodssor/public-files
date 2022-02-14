2d10c680cbbe667da2f77acf6ff6294441012819
nodssor <49564142+nodssor@users.noreply.github.com>
2022-02-14 13:06:09 -0500

    Update boostrap.sh

--- boostrap.sh
+++ boostrap.sh
@@ -1,16 +1,16 @@
 # This is to be run directly on new Linux installs
 # ex: curl -Lks https://raw.githubusercontent.com/nodssor/public-files/main/boostrap.sh
 /usr/bin/git clone --bare git@github.com:nodssor/.dotfiles.git $HOME/.dotfiles
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-dotfiles checkout
+dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+$dotfiles checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
     mkdir -p .config-backup
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-    dotfiles checkout
+    $dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    $dotfiles checkout
 fi;
 
-dotfiles config --local status.showUntrackedFiles no
+$dotfiles config --local status.showUntrackedFiles no
 echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
