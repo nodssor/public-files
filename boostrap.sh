70b74a806b07ad32a47d0b9608cdc7c07dec5554
nodssor <49564142+nodssor@users.noreply.github.com>
2022-06-09 13:55:23 -0400

    Update boostrap.sh
    Added git timeout

--- boostrap.sh
+++ boostrap.sh
@@ -1,12 +1,21 @@
 # This is to be run directly on new Linux installs
 # ex: curl -Lks https://raw.githubusercontent.com/nodssor/public-files/main/boostrap.sh | /bin/bash
 #set -x
+VERSION=3
+echo "Dotfile bootstrapper version: ${VERSION}"
+
 if [ ! -e /usr/bin/git ]; then
   echo "Requires git. [/usr/bin/git] not found."
   echo "Exiting."
   exit 1
 fi
-/usr/bin/git clone --bare git@github.com:nodssor/.dotfiles.git $HOME/.dotfiles
+
+timeout 10 /usr/bin/git clone --bare git@github.com:nodssor/.dotfiles.git $HOME/.dotfiles
+if [ $? = 124 ]; then
+  echo "Git clone timeout. Please make sure you have set up the SSH key correctly."
+  echo "Exiting."
+  exit 2
+fi
 dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 $dotfiles checkout > /dev/null 2>&1 
 if [ $? = 0 ]; then
